4fca7cca235ea289e4fe0af19244c7ca2c5940d5
Sir Abubakar <abudawud92@gmail.com>
2024-09-02 11:10:15 +0100

    upgraded repo

--- src/config-global.ts
+++ src/config-global.ts
@@ -10,6 +10,6 @@ export type ConfigValue = {
 // ----------------------------------------------------------------------
 
 export const CONFIG: ConfigValue = {
-  appName: 'POSSAP UI',
+  appName: 'POSSAP App',
   appVersion: packageJson.version,
 };

cee9a5e7600842d67e6200c4e8428daa8385ccaa
ImaanWada <62875755+ImaanWada@users.noreply.github.com>
2024-09-09 12:59:49 +0100

    forgot-password and reset-password

--- src/routes/sections.tsx
+++ src/routes/sections.tsx
@@ -18,6 +18,8 @@ export const HomePage = lazy(() => import('src/pages/home'));
 export const UserPage = lazy(() => import('src/pages/user'));
 export const SignInPage = lazy(() => import('src/pages/sign-in'));
 export const SignUpPage = lazy(() => import('src/pages/sign-up'));
+export const ForgotPassword = lazy(() => import('src/pages/forgot-password'));
+export const ResetPassword = lazy(() => import('src/pages/reset-password'));
 export const Page404 = lazy(() => import('src/pages/page-not-found'));
 
 // ----------------------------------------------------------------------
@@ -71,6 +73,22 @@ export function Router() {
       element: (
         <AuthLayout>
           <SignUpPage />
+        </AuthLayout>
+      ),
+    },
+    {
+      path: 'forgot-password',
+      element: (
+        <AuthLayout>
+          <ForgotPassword />
+        </AuthLayout>
+      ),
+    },
+    {
+      path: 'reset-password',
+      element: (
+        <AuthLayout>
+          <ResetPassword />
         </AuthLayout>
       ),
     },
---
+++ src/sections/auth/reset-password-view.tsx
@@ -0,0 +1,146 @@
+import * as Yup from 'yup';
+import React, { useState } from 'react';
+import { Form, Field, Formik } from 'formik';
+
+import { LoadingButton } from '@mui/lab';
+import {
+  Box,
+  Link,
+  Stack,
+  Button,
+  Checkbox,
+  Container,
+  TextField,
+  IconButton,
+  Typography,
+  InputAdornment,
+  FormControlLabel,
+} from '@mui/material';
+
+import { useRouter } from 'src/routes/hooks';
+
+import { Iconify } from 'src/components/iconify';
+
+export function ResetPasswordView() {
+  const [submitted, setsubmitted] = useState(false);
+  const router = useRouter();
+
+  const validationSchema = Yup.object().shape({
+    newPassword: Yup.string().min(8, 'Password must be at least 8 characters long').required('New password is required'),
+
+    repeatPassword: Yup.string().oneOf([Yup.ref('newPassword')], 'Passwords must match').required('Please confirm your new password'),
+    
+  });
+
+
+//   return (
+
+//     <Container maxWidth="lg" style={{ marginTop: '10px' }}>
+//     <Box display="flex" flexDirection="column" alignItems="center">
+//       <Typography variant="h5">Reset Password</Typography>
+//       <Typography variant="body2" color="textSecondary">
+//       A verification code will be sent to the specified email address
+//       </Typography>
+//     </Box>
+//     <Formik
+//       initialValues={{ email: ''}}
+//       validationSchema={validationSchema}
+//       onSubmit={(values) => {
+//         console.log('Form Data', values);
+//       }}
+//     >
+//       {({ errors, touched }) => (
+//         <Form>
+//           <Box mt={3}>
+//             <Field
+//               as={TextField}
+//               fullWidth
+//               name="email"
+//               label="Email"
+//               variant="outlined"
+//               error={touched.email && Boolean(errors.email)}
+//               helperText={touched.email && errors.email}
+//             />
+//           </Box>
+//           <Box mt={2} textAlign="center">
+//       <Typography variant="body2" color="textSecondary">
+//          Please enter your email.
+//       </Typography>
+//     </Box>
+//           <Box mt={3}>
+//             <LoadingButton
+//               fullWidth
+//               size="large"
+//               type="submit"
+//               color="primary"
+//               variant="contained"
+//             >
+//               Proceed
+//             </LoadingButton>
+//           </Box>
+//         </Form>
+//       )}
+//     </Formik>
+//   </Container>
+    
+//   );
+return (
+    <Container maxWidth="lg" style={{ marginTop: '10px' }}>
+      <Box display="flex" flexDirection="column" alignItems="center">
+        <Typography variant="h5">Reset Password</Typography>
+      </Box>
+      <Formik
+        initialValues={{  newPassword: '', repeatPassword: '' }}
+        validationSchema={validationSchema}
+        onSubmit={(values) => {
+          console.log('Form Data', values);
+        }}
+      >
+        {({ errors, touched }) => (
+          <Form>
+            <Box mt={3}>
+              <Field
+                as={TextField}
+                fullWidth
+                name="newPassword"
+                type="password"
+                label="New Password"
+                variant="outlined"
+                error={touched.newPassword && Boolean(errors.newPassword)}
+                helperText={touched.newPassword && errors.newPassword}
+              />
+            </Box>
+  
+            <Box mt={3}>
+              <Field
+                as={TextField}
+                fullWidth
+                name="repeatPassword"
+                type="password"
+                label="Confirm New Password"
+                variant="outlined"
+                error={touched.repeatPassword && Boolean(errors.repeatPassword)}
+                helperText={touched.repeatPassword && errors.repeatPassword}
+              />
+            </Box>
+  
+              <Box mt={3}>
+              <LoadingButton
+                fullWidth
+                size="large"
+                type="submit"
+                color="primary"
+                variant="contained"
+              >
+                Reset
+              </LoadingButton>
+            </Box>
+          </Form>
+        )}
+      </Formik>
+    </Container>
+  );
+  
+
+
+}

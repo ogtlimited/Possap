9ead57a652da340580db2c5ed8fc4d3bc11cab7a
ImaanWada <62875755+ImaanWada@users.noreply.github.com>
2024-09-09 13:06:07 +0100

    removed commented-out line

--- src/sections/auth/reset-password-view.tsx
+++ src/sections/auth/reset-password-view.tsx
@@ -32,58 +32,6 @@ export function ResetPasswordView() {
     
   });
 
-
-//   return (
-
-//     <Container maxWidth="lg" style={{ marginTop: '10px' }}>
-//     <Box display="flex" flexDirection="column" alignItems="center">
-//       <Typography variant="h5">Reset Password</Typography>
-//       <Typography variant="body2" color="textSecondary">
-//       A verification code will be sent to the specified email address
-//       </Typography>
-//     </Box>
-//     <Formik
-//       initialValues={{ email: ''}}
-//       validationSchema={validationSchema}
-//       onSubmit={(values) => {
-//         console.log('Form Data', values);
-//       }}
-//     >
-//       {({ errors, touched }) => (
-//         <Form>
-//           <Box mt={3}>
-//             <Field
-//               as={TextField}
-//               fullWidth
-//               name="email"
-//               label="Email"
-//               variant="outlined"
-//               error={touched.email && Boolean(errors.email)}
-//               helperText={touched.email && errors.email}
-//             />
-//           </Box>
-//           <Box mt={2} textAlign="center">
-//       <Typography variant="body2" color="textSecondary">
-//          Please enter your email.
-//       </Typography>
-//     </Box>
-//           <Box mt={3}>
-//             <LoadingButton
-//               fullWidth
-//               size="large"
-//               type="submit"
-//               color="primary"
-//               variant="contained"
-//             >
-//               Proceed
-//             </LoadingButton>
-//           </Box>
-//         </Form>
-//       )}
-//     </Formik>
-//   </Container>
-    
-//   );
 return (
     <Container maxWidth="lg" style={{ marginTop: '10px' }}>
       <Box display="flex" flexDirection="column" alignItems="center">
